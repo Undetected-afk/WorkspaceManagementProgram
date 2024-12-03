c4289a7e1ad9c2ae126f4ce3435ff1b00728528f
Undetected-afk <rejirahulv4@gmail.com>
2024-12-03 00:27:54 -0500

    Update Node.py

--- src/Node.py
+++ src/Node.py
@@ -1,8 +1,7 @@
-# Node.py
+# TaskNode.py
 class Node:
     def __init__(self, data=None):
-        self.data = data  # This could be any data, like a name or any other attribute
-        self.name = data  # Store the name as well for easier access (assuming the name is passed as data)
+        self.data = data
 
     def __repr__(self):
-        return f"Node({self.name})"  # Show the name for clarity
+        return f"Node({self.data})"
