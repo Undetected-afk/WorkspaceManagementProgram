934414b529722a12325cdf0ca9c581ac240fe534
Undetected-afk <rejirahulv4@gmail.com>
2024-12-02 23:42:22 -0500

    Update FileSystemDriver.py
    Create two folders (folder1 and folder2) and three files (file1.txt, file2.txt, and file3.pdf).
    Files are added to the corresponding folders using the add_file() method.
    Finally, the script prints out the folder name and the list of files it contains.

--- src/FileSystemDriver.py
+++ src/FileSystemDriver.py
@@ -1,8 +1,20 @@
+# FileSystemDriver.py
 from FileSystem import FolderNode, FileNode
 
-# Create folder and file structure
-folder1 = FolderNode("Folder 1")
-file1 = FileNode("File 1")
-folder1.add_file(file1)
+# Create folders and files
+folder1 = FolderNode("Documents")
+folder2 = FolderNode("Downloads")
 
+file1 = FileNode("file1.txt")
+file2 = FileNode("file2.txt")
+file3 = FileNode("file3.pdf")
+
+# Add files to folders
+folder1.add_file(file1)
+folder1.add_file(file2)
+
+folder2.add_file(file3)
+
+# Print folder information
 print(f"Folder: {folder1.name}, Files: {[file.name for file in folder1.files]}")
+print(f"Folder: {folder2.name}, Files: {[file.name for file in folder2.files]}")
